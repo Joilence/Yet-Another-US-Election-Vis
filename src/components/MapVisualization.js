c9ef7a67295c16ccfa5c0fc99d18866010ab6d80
Joilence <cjyk84719@gmail.com>
2020-12-24 10:23:24 +0100

    remove this.USStateNames

--- src/components/MapVisualization.js
+++ src/components/MapVisualization.js
@@ -11,14 +11,6 @@ export default class MapVisualzation {
     this.datasets = datasets;
     this.usaMap = {};
     this.dataset = [];
-    this.USStateNames = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut"
-                      , "delaware", "district-of-columbia", "florida", "georgia", "idaho", "illinois", "indiana"
-                      , "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan"
-                      , "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire"
-                      , "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma"
-                      , "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee"
-                      , "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming", "hawaii"]; //puerto-rico
-
     this.USStatesCoordinate = UsMapGeoJson.features;
     this.USStatesCoordinate_Dict = {}; // {"name":[coordinates]}
 
